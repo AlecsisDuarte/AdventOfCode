a04d1aa6b9f65522f29de4fb638545cfa69266ea
Alexis Duarte <alexis.duarte@hotmail.com>
2016-12-03 00:21:53 -0800

    Finished day1 problem

--- Day1/blocksAway.java
+++ Day1/blocksAway.java
@@ -1,23 +1,56 @@
 import java.util.Scanner;
-package default;
 
-public class blocksAway{
-  public static void main(String[] args){
-    Scanner tcl = new Scanner(System.in);
-    int cantBlocks=0;
-    int direction[2]={0,0};
-    char aux=' ';
-    String[] blocks = tcl.nextLine().split(", ");
-    for(int i =0; i<blocks.length; i++){
-      if(blocks[i].startsWith(aux+"")){
-        cantBlocks-= Integer.parseInt(blocks[i].substring(1));
-        aux = ' ';
-      }else{
-        if(blocks[i].startsWith("L"))
-          cantBlockes[Integer.parseInt(blocks[i].substring(1));
-        aux = blocks[i].charAt(0);
-      }
-    }
-    System.out.println("Cantidad de Bloques = "+cantBlocks);
-  }
+public class blocksAway {
+	public static void main(String[] args) {
+		Scanner tcl = new Scanner(System.in);
+	    int x = 0 ,y = 0;
+	    char lastDir=' ';
+	    String[] blocks = tcl.nextLine().split(", ");
+	    for(int i =0; i<blocks.length; i++){
+	    	int amountOfBlocks = Integer.parseInt(blocks[i].substring(1));
+	    	switch(blocks[i].charAt(0)){
+	    	case 'R':
+	    		if(lastDir == 'R'){
+	    			x-=amountOfBlocks;
+	    			lastDir= 'S'; //Because you'll be facing south
+	    		}
+	    		else if(lastDir == 'L'){
+	    			x+=amountOfBlocks;
+	    			lastDir= ' '; //You'll be facing north, so it restarts
+	    		}
+	    		else if(lastDir == 'S'){
+	    			y-=amountOfBlocks;
+	    			lastDir = 'R'; //You'll be facing west
+	    		}
+	    		else{
+	    			y+=amountOfBlocks;
+	    			lastDir = 'R'; //You'll be facing east
+	    		}
+	    		break;
+	    	case 'L':
+	    		if(lastDir == 'R'){
+	    			x+=amountOfBlocks;
+	    			lastDir= ' '; //You'll be facing north, so it restarts
+	    		}
+	    		else if(lastDir == 'L'){
+	    			x-=amountOfBlocks;
+	    			lastDir= 'S'; //Because you'll be facing south
+	    		}
+	    		else if(lastDir == 'S'){
+	    			y+=amountOfBlocks;
+	    			lastDir = 'L'; //You'll be facing east
+	    		}
+	    		else{
+	    			y-=amountOfBlocks;
+	    			lastDir = 'L'; //You'll be facing west
+	    		}
+	    		break;
+	    	}
+	    }
+	    System.out.println("("+x+","+y+")");
+	    x = Math.abs(x);
+	    y = Math.abs(y);
+	    System.out.format("%d blocks away", x+y);
+	}
+
 }
