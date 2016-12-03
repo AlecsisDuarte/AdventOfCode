0122101514cbe3538161ab0968a8ae6f05bffe45
Alexis Duarte <alexis.duarte@hotmail.com>
2016-12-02 21:55:02 -0800

    The actual Day1, not finished

---
+++ Day1/blocksAway.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class blocksAway{
+  public static void main(String[] args){
+    Scanner tcl = new Scanner(System.in);
+    int cantBlocks=0;
+    int direction[2]={0,0};
+    char aux=' ';
+    String[] blocks = tcl.nextLine().split(", ");
+    for(int i =0; i<blocks.length; i++){
+      if(blocks[i].startsWith(aux+"")){
+        cantBlocks-= Integer.parseInt(blocks[i].substring(1));
+        aux = ' ';
+      }else{
+        if(blocks[i].startsWith("L"))
+          cantBlockes[Integer.parseInt(blocks[i].substring(1));
+        aux = blocks[i].charAt(0);
+      }
+    }
+    System.out.println("Cantidad de Bloques = "+cantBlocks);
+  }
+}
