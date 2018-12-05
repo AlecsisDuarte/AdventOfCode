7a32ad61b50373c748f9a0f61a6f448b3db78de0
lduenas <luis.alonso.duenas.m@gmail.com>
2018-12-04 16:01:16 -0800

    Added day4 part2 solution

--- 2018/Day4/repose_record.dart
+++ 2018/Day4/repose_record.dart
@@ -1,9 +1,10 @@
+import 'dart:collection';
 import 'dart:io';
 
 import '../utils/input_reader.dart';
 
 RegExp logRegex = new RegExp(r"(\d*)-(\d*)-(\d*)\ (\d*):(\d*).+?(\d+|f|w)");
-var datetimeGroupList = [1,2,3,4,5];
+var datetimeGroupList = [1, 2, 3, 4, 5];
 
 void main() async {
   File input = InputReader.openInputFile('Day4', 'input.txt');
@@ -12,60 +13,103 @@ void main() async {
     return;
   }
 
-  input
-    .readAsLines()
-    .then((lines) {
-      var sleepTimes = getSleepTimes(lines);
-      getStrategy(sleepTimes)
-      .then((strategy) => print('Strategy: $strategy'));
-    });
+  input.readAsLines().then((lines) {
+    var sleepTimes = getSleepTimes(lines);
+    getStrategy(Map.from(sleepTimes)).then((strategy) => print(
+        'Strategy for the guard that slept more than any other: $strategy'));
 
+    getMoreSleptsInMinute(Map.from(sleepTimes)).then((strategy) => print(
+        'Strategy for the guard that slept more in the same minute: $strategy'));
+  });
 }
 
-
-Map<int,Times> getSleepTimes(List<String> logs) {
-  var records = Map<int,String>();
+/** Returns how many times the guards slept in each hour:minute */
+Map<String, Times> getSleepTimes(List<String> logs) {
+  var recordsList = List<LogInfo>();
   logs.forEach((log) {
     var logMatch = logRegex.firstMatch(log);
-    int dateTime = int.parse(logMatch.groups(datetimeGroupList).reduce((line, part) => line += part));
+    int dateTime = int.parse(logMatch
+        .groups(datetimeGroupList)
+        .reduce((line, part) => line += part));
     String info = logMatch.group(6);
-
-    records.putIfAbsent(dateTime, () => info);
+    recordsList.add(LogInfo(dateTime, info));
   });
 
-  var sleepTimes = Map<int,Times>();
-  var timeStamps = records.keys.toList();
+  recordsList.sort((a, b) => a.datetime.compareTo(b.datetime));
+  var sleepTimes = Map<String, Times>();
 
-  for(int index = 0; index < timeStamps.length; index++) {
-    int timeStamp = timeStamps[index];
-    int guard = int.parse(records[timeStamp]);
-    do {
-      int initSleepTime = timeStamps[++index];
-      int wakeTime = timeStamps[++index];
-      for(int sleepTime = initSleepTime; sleepTime < wakeTime; sleepTime++) {
-        sleepTimes.update(
-          sleepTime, 
-          (update) { ++update.times; },
-          ifAbsent: () => Times(guard, 1));
-      }
-    } while(index + 1 < timeStamps.length && records[timeStamps[index + 1]] == 'f');
+  for (int index = 0; index < recordsList.length; index++) {
+    int guard = int.parse(recordsList[index].info);
+    if (index < recordsList.length - 2 && recordsList[index + 1].info == 'f') {
+      do {
+        int hourMinSleep = recordsList[++index].datetime % 10000;
+        int hourMinWake = recordsList[++index].datetime % 10000;
+
+        for (int sleepTime = hourMinSleep;
+            sleepTime != hourMinWake;
+            sleepTime++) {
+          if (sleepTime == 2359) sleepTime = 0;
+          sleepTimes.update(
+              "$sleepTime-$guard", (time) => Times(guard, time.times + 1),
+              ifAbsent: () => Times(guard, 1));
+        }
+      } while (
+          index + 1 < recordsList.length && recordsList[index + 1].info == 'f');
+    }
   }
-  
+
   return sleepTimes;
 }
 
-Future<int> getStrategy(Map<int, Times> sleepTimes) async {
+Future<int> getStrategy(Map<String, Times> sleepTimes) async {
   int minute = 0;
+  var guardSleepTime = Map<int, int>();
 
-  Times maxTime = sleepTimes.values.reduce((maxTime, time) => maxTime = maxTime.times > time.times? maxTime : time);
-  sleepTimes.forEach((dt,time){
-    if (time == maxTime) {
-      minute = (dt % 100);
-      return;
+  sleepTimes.values.forEach((guardTimes) => guardSleepTime.update(
+      guardTimes.guard, (timeSlept) => timeSlept += guardTimes.times,
+      ifAbsent: () => guardTimes.times));
+
+  int maxSleepingGuard =
+      guardSleepTime.values.reduce((max, cur) => max = max > cur ? max : cur);
+
+  int guard = guardSleepTime.entries
+      .where((gs) => gs.value == maxSleepingGuard)
+      .first
+      .key;
+
+  String guardTimeInfo = "";
+
+  final String strGuard = guard.toString();
+
+  sleepTimes.removeWhere((timeGuard, times) => !timeGuard.endsWith(strGuard));
+
+  var maxSleepTimes = sleepTimes.values.reduce((maxTimes, times) =>
+      maxTimes = maxTimes.times > times.times ? maxTimes : times);
+
+  sleepTimes.forEach((time, times) {
+    if (times == maxSleepTimes) {
+      guardTimeInfo = time;
     }
   });
 
-  return minute * maxTime.guard;
+  var time = guardTimeInfo.split('-')[0];
+  minute = int.parse(time.length > 1 ? time.substring(time.length - 2) : time);
+
+  return minute * guard;
+}
+
+/** Returns the result for the guard that slept more times in the same minute */
+Future<int> getMoreSleptsInMinute(Map<String, Times> sleepTimes) async {
+  Times maxSleptGuardTimes = sleepTimes.values
+      .reduce((max, curr) => max = max.times > curr.times ? max : curr);
+  String guardTimestamp = '';
+  sleepTimes.removeWhere((guardTime, times) => times != maxSleptGuardTimes);
+  sleepTimes.forEach((guardTime, times) => guardTimestamp = guardTime);
+
+  var time = guardTimestamp.split('-')[0];
+  var minute =
+      int.parse(time.length > 1 ? time.substring(time.length - 2) : time);
+  return maxSleptGuardTimes.guard * minute;
 }
 
 class Times {
@@ -73,4 +117,10 @@ class Times {
   int times;
 
   Times(this.guard, this.times);
+}
+
+class LogInfo {
+  int datetime;
+  String info;
+  LogInfo(this.datetime, this.info);
 }
